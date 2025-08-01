ad29ca62eea050000b882b133eef7454801ba63a
Juan Avalos <111295746+juanvaloss@users.noreply.github.com>
2025-07-31 18:02:48 -0600

    JWT Auth

--- watchmate/user_app/api/urls.py
+++ watchmate/user_app/api/urls.py
@@ -1,4 +1,6 @@
 from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 from django.urls import path
 
 from user_app.api.views import registrations_view, logout_view
@@ -8,4 +10,9 @@ urlpatterns = [
     path('login/', obtain_auth_token, name='login'),
     path('register/', registrations_view, name='register'),
     path('logout/', logout_view, name='logout'),
+    
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    
+    
 ]
